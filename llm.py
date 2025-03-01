47308cca3aef6607533599a1bd7f7caa85c0f885
Thomas M <thomasm6m6@gmail.com>
2025-03-01 14:04:13 -0800

    Add persistent notes

--- llm.py
+++ llm.py
@@ -4,10 +4,9 @@ import pty
 import sys
 import time
 import signal
+import string
 from datetime import datetime
 from openai import OpenAI
-
-# TODO: maintain a plan external to the message history.
 
 client = OpenAI(
     api_key=os.environ["GEMINI_API_KEY"],
@@ -16,6 +15,23 @@ client = OpenAI(
 
 system_prompt = {"role": "system", "content": "You are playing Colossal Cave Adventure. 'User' is the game. Response format: 'move: word1 word2' (word2 is optional)."}
 messages = [system_prompt]
+
+prompt = string.Template("""\
+<moves>
+$moves
+</moves>
+
+<notes>
+$notes
+</notes>
+
+You are playing Colossal Cave Adventure.
+Above are the last 100 moves of the game, with the most recent move last.
+Followed by that is your notes document, aka your scratch space. This serves as your short term memory; you should store everything of possible relevance here.
+Decide on a move and return it, prefixed with "move:". Then update your notes and return them, prefixed with "notes:".
+Make sure to think through each move, carefully considering all options and their possible outcomes before choosing a move.
+If you're confused about how to do something, move on from it.
+""")
 
 now = int(time.time())
 OUTFILE = open(f"logs/{now}.txt", 'w')
@@ -28,44 +44,61 @@ def out(msg):
     print(msg, file=OUTFILE, end='', flush=True)
     print(msg, end='', flush=True)
 
-class Message:
-    def __init__(self, role, content):
-        self.role = role
-        self.content = content
+class Move:
+    def __init__(self, move, result):
+        self.move = move
+        self.result = result
 
-    def asDict(self):
-        return [{"role": self.role, "content": self.content}]
+    def __str__(self):
+        if self.move == "":
+            return self.result
+        return f"> {self.move}\n\n{self.result}"
 
-def get_move(messages):
+def get_move(moves, notes, extra_prompt=""):
     try:
         log("requesting chat completion...")
+
+        prompt_str = prompt.substitute(
+            moves='\n\n'.join(map(lambda x: str(x), moves)),
+            notes=notes
+        )
+        if extra_prompt:
+            prompt_str += '\n\n' + extra_prompt
+        log(f"prompt = {prompt_str}")
+
         response = client.chat.completions.create(
-            model="gemini-2.0-flash-thinking-exp",
+            model="gemini-2.0-pro-exp",
             n=1,
-            messages=messages,
+            messages=[{"role": "user", "content": prompt_str}],
             timeout=10
         )
         content = response.choices[0].message.content
         log(f"received chat completion ({content})...")
         if content is None:
             print("Error, running again: content is null", file=LOGFILE, flush=True)
-            return get_move(messages)
+            return get_move(moves, notes)
 
         content = content.strip('.,!?` \t')
-        match = re.search(r'move:((\s+\w+\b)+)$', content, re.ASCII)
-        if not match or match.group(1) is None:
-            log(f"Error, running again: move unspecified (content: {content})")
-            move_msg = Message("assistant", match.group() if match else "")
-            resp_msg = Message("user", "ERROR: badly formatted move.")
-            return get_move(messages + [move_msg.asDict(), resp_msg.asDict()])
 
-        cmd = match.group(1).lstrip()
-        log(f"Parsed move as '{cmd}'")
-        return cmd
+        move_match = re.search(r'move:((\s*\w+\b)+)$', content, re.ASCII | re.MULTILINE)
+        if not move_match or move_match.group(1) is None:
+            log(f"Error, running again: bad formatting for move (content: {content}))")
+            return get_move(moves, notes)
+        new_move = move_match.group(1).strip()
+        log(f"Parsed move as '{new_move}'")
+
+        notes_match = re.search(r'notes:(.*)', content, re.DOTALL)
+        if not notes_match or notes_match.group(1) is None:
+            log(f"Error, running again: bad formatting for notes (content: {content}))")
+            return get_move(moves, notes)
+        new_notes = notes_match.group(1).strip()
+        log(f"Parsed notes as '{new_notes}'")
+
+        return new_move, new_notes
     except Exception as error:
         log(f"Error, waiting for 5 seconds (error: {error})")
         time.sleep(5)
-        return get_move(messages)
+        return get_move(moves, notes)
 
 
 master, slave = pty.openpty()
@@ -125,8 +158,11 @@ if pid == 0:
     os.execvp("advent", ["advent"])
 else:
     os.close(slave)
+    moves = []
+    notes = ""
     last_call = 0
     buffer = ""
+    move = ""
     while True:
         try:
             output = os.read(master, 1024).decode()
@@ -135,28 +171,23 @@ else:
             if buffer.endswith("> "):
                 ready = True
                 out(buffer)
-                messages.append({
-                    "role": "user",
-                    "content": buffer.rstrip("> ").strip()
-                })
+                result = buffer.lstrip(move).rstrip("> ").strip()
+                moves.append(Move(move, result))
 
                 now = time.time()
-                if now - last_call < 3:
-                    time.sleep(3 - (now - last_call))
+                if now - last_call < 4:
+                    time.sleep(4 - (now - last_call))
                 last_call = time.time()
 
-                move = get_move(messages)
+                # log(f"calling get_move({notes}, {moves[-100:]})")
+                move, notes = get_move(moves[-100:], notes)
                 if not ready: # SIGINT
                     break
                 ready = False
                 send_move(move)
 
-                messages.append({
-                    "role": "assistant",
-                    "content": "move: " + move
-                })
                 buffer = ""
-            elif output == "":
+            elif output == "": # XXX delete?
                 log("Done")
                 break
         except OSError as e:
